32f5a649555ec91a5ab9343baf8de37ee54b6c24
Jonathan <jonathanfelipy20@gmail.com>
2026-01-28 12:11:47 -0300

    Refactor greeting message with color formatting

--- Exercicio01.py
+++ Exercicio01.py
@@ -1,14 +1,14 @@
 #0 Crie um comando para dizer 'Olá Mundo' na tela
 
-print('Olá Mundo!')
+print('\033[34mOlá Mundo!')
 
 #0.0 Crie uma forma secundária de escrever a mensagem 'Olá Mundo' na tela
 
 msg = 'Olá Mundo!'
 print(msg)
 
-#1 - Crie um Script Python que leia o nome de uma pessoa
-#    e mostrar uma mensagem de boas-vindas dde acordo com o valor digitado.
+# 1 - Crie um Script Python que leia o nome de uma pessoa
+# e mostrar uma mensagem de boas-vindas de acordo com o valor digitado.
 
 nome = input('Qual o seu nome? ')
 print('Seja bem-vindo ao meu mundo, Sr.(a)' + nome + '!')
