bd2f22e6318fa3aad8a46cdd37845f4384ec6e14
Jonathan <jonathanfelipy20@gmail.com>
2026-01-28 12:12:18 -0300

    Enhance welcome message with color formatting

--- Exercicio02.py
+++ Exercicio02.py
@@ -1,10 +1,10 @@
 #2 - Crie um script Python que leia o dia, o mês e o ano de nascimento de uma pessoa
 #    e mostre uma mensagem com a data formada.
 
-print('Bem-vindo, pessoa! Me diga o dia, mês e ano do seu nascimento para que eu os formate para ti!')
+print('\033[33mBem-vindo, pessoa! Me diga o dia, mês e ano do seu nascimento para que eu os formate para ti!\033[m')
 
 ano = input('Qual o ano de seu Nascimento? ')
 mes = input('Qual seu mês de Nascimento (escreva por extenso)? ')
 dia = input('Qual sua data de Nascimento? ')
 
-print('Então, você nasceu no dia ' + dia + ' de ' + mes + ' em ' + ano + '!')
+print('\033[7;42mEntão, você nasceu no dia ' + dia + ' de ' + mes + ' em ' + ano + '!\033[m')
