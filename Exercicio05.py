bdba9aec5a8823e9ea05b591787d65ca50a71f4e
Jonathan <jonathanfelipy20@gmail.com>
2026-01-28 12:13:23 -0300

    Enhance input/output formatting with colors

--- Exercicio05.py
+++ Exercicio05.py
@@ -1,7 +1,5 @@
 # Exercício 5 Faça um programa que leia um número inteiro e mostre na tela
 # o seu sucessor e seu antecessor.
 
-n1 = int(input('Digite um valor para mostrar-lhes seu sucessor e seu antecessor: '))
-print('O valor posto é este: {}. Logo, seu antecessor é {} e seu sucessor é {}.' .format(n1, n1-1, n1+1))
-
-
+n1 = int(input('\033[1;31;44mDigite um valor para mostrar-lhes seu sucessor e seu antecessor: \033[m'))
+print('O valor posto é este: \033[33m{}\033[m. Logo, seu antecessor é \033[35m{}\033[m e seu sucessor é \033[35m{}\033[m.' .format(n1, n1-1, n1+1))
